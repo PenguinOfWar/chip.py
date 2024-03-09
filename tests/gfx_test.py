38ab2ae9b2f3f15e0dd61e2af582f1181df67050
Darryl Walker <darryljwalker@me.com>
2024-03-09 16:42:58 +0000

    bug: trying to fix no $DISPLAY on tkinter on github actions

--- tests/gfx_test.py
+++ tests/gfx_test.py
@@ -1,6 +1,11 @@
+import os
 import unittest
 
 from drivers.gfx import Gfx
+
+if os.environ.get("DISPLAY", "") == "":
+    print("no display found. Using :0.0")
+    os.environ.__setitem__("DISPLAY", ":0.0")
 
 
 class TestGfx(unittest.TestCase):
